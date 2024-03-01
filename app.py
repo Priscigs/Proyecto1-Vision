a9d3d6f2f94ed12d4fc9a512e0bc8f28be5f9a99
Estef072 <61289015+Estef072@users.noreply.github.com>
2024-02-29 22:36:37 -0600

    Final!!

--- app.py
+++ app.py
@@ -1,8 +1,24 @@
 import cv2
 from matplotlib import pyplot as plt
+import numpy as np
+
+# Función para calcular métricas
+def calculate_metrics(true_mask, predicted_mask):
+    true_positive = np.sum(np.logical_and(predicted_mask == 255, true_mask == 255))
+    false_positive = np.sum(np.logical_and(predicted_mask == 255, true_mask == 0))
+    true_negative = np.sum(np.logical_and(predicted_mask == 0, true_mask == 0))
+    false_negative = np.sum(np.logical_and(predicted_mask == 0, true_mask == 255))
+
+    accuracy = (true_positive + true_negative) / (true_positive + false_positive + true_negative + false_negative)
+    recall = true_positive / (true_positive + false_negative)
+    specificity = true_negative / (true_negative + false_positive)
+    precision = true_positive / (true_positive + false_positive)
+    f1_score = (2 * precision * recall) / (precision + recall)
+
+    return accuracy, recall, specificity, precision, f1_score
 
 # Cargar la imagen en escala de grises
-image_path = 'pictures/16.pgm'  # Cambia esto por la ruta correcta de tu imagen
+image_path = 'pictures/2.pgm'  # Cambia esto por la ruta correcta de tu imagen
 image = cv2.imread(image_path, cv2.IMREAD_GRAYSCALE)
 
 # Preprocesamiento: Reducir ruido con un filtro mediano
@@ -25,6 +41,19 @@ img_inverted_clahe = cv2.bitwise_not(img_adaptive_mean_clahe)
 kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (3, 3))
 # Utilizar apertura morfológica para eliminar ruido pequeño
 img_morph = cv2.morphologyEx(img_inverted_clahe, cv2.MORPH_OPEN, kernel)
+
+# Cargar la máscara de referencia
+true_mask_path = 'pictures/_gt.pgm'  # Ruta de la máscara de referencia
+true_mask = cv2.imread(true_mask_path, cv2.IMREAD_GRAYSCALE)
+
+# Calcular métricas
+accuracy, recall, specificity, precision, f1_score = calculate_metrics(true_mask, img_morph)
+
+print("Accuracy:", accuracy)
+print("Recall:", recall)
+print("Specificity:", specificity)
+print("Precision:", precision)
+print("F1 Score:", f1_score)
 
 # Mostrar la imagen original y la imagen final procesada
 fig, axs = plt.subplots(1, 2, figsize=(10, 5), sharey=True)
